policy gradient and environment correct

RL_algorithms/DQN.py
@@ -6,10 +6,11 @@ import random
 from collections import deque
 from neural_networks.FFNN import FFNN
 from option_hedging.code_pytorch.DeepHedgingEnvironment import DeepHedgingEnvironment
+import torch.optim.lr_scheduler as lr_scheduler
 
 # Double DQN agent
 class DoubleDQN:
-    def __init__(self, state_size, action_size, num_layers, hidden_size, gamma=0.99, epsilon=1.0, epsilon_min=0.01, epsilon_decay=0.99998, lr=0.001, batch_size=64, target_update=10):
+    def __init__(self, state_size, action_size, num_layers, hidden_size, gamma=0.999, epsilon=1.0, epsilon_min=0.01, epsilon_decay=0.99998, lr=0.001, batch_size=512, target_update=10):
         self.state_size = state_size
         self.action_size = action_size
         self.gamma = gamma                      # discount factor
@@ -18,7 +19,7 @@ class DoubleDQN:
         self.epsilon_decay = epsilon_decay      # decay rate of epsilon
         self.lr = lr                            # learning rate 
         self.batch_size = batch_size            # batch size    
-
+        self.target_update = target_update      # Frequency at which target model is updated
         # Experience replay buffer
         self.memory = deque(maxlen=2000)
         
@@ -45,7 +46,7 @@ class DoubleDQN:
             return torch.randint(self.action_size, size=(1,))
         with torch.no_grad():
             action_values = self.model(state)
-        return torch.argmin(action_values, dim=1)
+        return torch.argmax(action_values, dim=1)
 
     # perform an update based on a mini batch sampled from the replay memory buffer
     def replay(self):
@@ -71,7 +72,7 @@ class DoubleDQN:
         q_value = q_values.gather(1, actions)
 
         # pick the next action greedily
-        next_action = torch.argmin(next_q_values, dim=1).unsqueeze(1)
+        next_action = torch.argmax(next_q_values, dim=1).unsqueeze(1)
         # q_values of the next actions picked in the minibatch
         target_q_value = next_q_target.gather(1, next_action)
 
@@ -99,73 +100,89 @@ class DoubleDQN:
         torch.save(self.model.state_dict(), name)
 
 # Training loop
-def train_dqn(model, env, episodes=200, target_update_freq=10):
-    agent = model
-    agent.model.train()
-    env.train()
+    def train(self, env, episodes=200, lr_schedule = True):
+        self.model.train()
+        env.train()
 
-    print("TRAINING DQN: ")
+        if lr_schedule:
+            self.scheduler = lr_scheduler.LinearLR(self.optimizer, start_factor=1.0, end_factor=0.1, total_iters=episodes)
 
-    for e in range(episodes):
-        state = env.reset()
-        done = False
-        total_reward = 0
-        
-        while not done:
-            action = agent.act(state)
-            next_state, reward, done = env.step(action)
-            reward = torch.square(torch.where(reward > 0, reward, 0))/100
-            agent.remember(state, action, reward, next_state, done)
-            state = next_state
+        print("TRAINING DQN: ")
 
-            total_reward += reward[0].item()
+        for e in range(episodes):
+            state = env.reset()
+            done = torch.zeros(1)
+            total_reward = torch.zeros(1, device=self.device)
             
-            agent.replay()
+            while torch.all(done == 0):
+                action = self.act(state)
+                next_state, reward, done = env.step(action)
+                reward = -torch.square(torch.where(reward > 0, reward, 0))
+                self.remember(state, action, reward, next_state, done)
+                state = next_state
+
+                total_reward += reward
+                
+                self.replay()
+            
+            if e % self.target_update == 0:
+                self.update_target_model()
+            
+            if lr_schedule:
+                self.scheduler.step()
+
+            print(f"Episode {e+1}/{episodes}, Epsilon: {self.epsilon}, Total Reward: {-total_reward.item()}")
+
+        self.save("dqn_model.pth")
+
+    def test(self, env):
+        """
+        Test a trained DQN agent on the environment.
         
-        if e % target_update_freq == 0:
-            agent.update_target_model()
-        
-        print(f"Episode {e+1}/{episodes}, Epsilon: {agent.epsilon}, Total Reward: {total_reward}")
+        Args:
+        - env: The environment to test on.
+        - episodes: Number of episodes to test the agent.
+        - batch_size: Batch size.
+        Returns:
+        - total_rewards: List of total rewards for each episode.
+        """
+        self.model.eval()
+        env.test()
+        train_size = env.dataset.shape[0]
+        num_points = env.N
+        episodes = int(train_size/self.batch_size)
 
-    agent.save("dqn_model.pth")
+        actions = torch.zeros(num_points, self.batch_size, episodes, device=self.device)
+        rewards = torch.zeros(self.batch_size, episodes, device=self.device)
 
-def test_dqn(model, env, episodes=100, batch_size=512):
-    """
-    Test a trained DQN agent on the environment.
-    
-    Args:
-    - env: The environment to test on.
-    - agent: The trained DQN agent.
-    - episodes: Number of episodes to test the agent.
-    
-    Returns:
-    - total_rewards: List of total rewards for each episode.
-    """
-    env.test()
+        print("TESTING DQN: ")
 
-    print("TESTING DQN: ")
+        for episode in range(episodes):
+            state = env.reset(self.batch_size)  # Initialize the environment and get the initial state
+            done = torch.zeros(self.batch_size)
+            total_reward = torch.zeros(self.batch_size, device=self.device)
+            
+            i = 1
+            while torch.all(done == 0):
 
-    for episode in range(episodes):
-        state = env.reset(batch_size)  # Initialize the environment and get the initial state
-        done = torch.zeros(batch_size)
-        total_reward = torch.zeros(batch_size)
+                # Get the action from the trained model (greedy policy, no epsilon-greedy)
+                with torch.no_grad():
+                    q_values = self.model(state)
+                    action = torch.argmax(q_values, dim=1)
 
-        agent = model
-        agent.model.eval()
-        
-        while torch.all(done == 0):
+                # Step the environment with the chosen action
+                next_state, reward, done = env.step(action)
+                # Store action
+                actions[i,:,episode] = action.flatten()
+                # Store reward (hedging error)
+                rewards[:,episode] = reward
+                # Accumulate the reward
+                total_reward += reward
+                # Move to the next state
+                state = next_state
 
-            # Get the action from the trained model (greedy policy, no epsilon-greedy)
-            with torch.no_grad():
-                q_values = agent.model(state)
-                action = torch.argmin(q_values, dim=1)
-
-            # Step the environment with the chosen action
-            next_state, reward, done = env.step(action)
-            # Accumulate the reward
-            total_reward = total_reward + reward.cpu()
-            # Move to the next state
-            state = next_state
-        
-        rsmse = torch.sqrt(torch.mean(torch.square(torch.where(total_reward > 0, total_reward, 0)))).item()
-        print(f"Episode {episode+1}/{episodes}, rsmse: {rsmse}")
+                i += 1
+            
+            loss = torch.sqrt(torch.mean(torch.square(torch.where(total_reward > 0, total_reward, 0))))
+            print(f"Episode {episode+1}/{episodes}, Total Reward: {loss}")
+            return actions.flatten(1), rewards.flatten()

neural_networks/FFNN.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 
 class FFNN(nn.Module):
     
-    def __init__(self, in_features, out_features, num_layers, hidden_size, dropout=0.5):
+    def __init__(self, in_features, out_features, num_layers, hidden_size, dropout=0.0):
         
         super().__init__()
         

neural_networks/LSTM.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 
 class LSTM_multilayer_cell(nn.Module):
     
-    def __init__(self, batch_size, input_size, hidden_size, num_layers, device, dropout=0.5):
+    def __init__(self, batch_size, input_size, hidden_size, num_layers, device, dropout=0.0):
         
         super().__init__()
 

neural_networks/Transformer.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 
 class Transformer(nn.Module):
     
-    def __init__(self, in_features, seq_length, d_model, dim_feedforward, n_heads, num_layers, dropout=0.5):
+    def __init__(self, in_features, seq_length, d_model, dim_feedforward, n_heads, num_layers, dropout=0.0):
         
         super().__init__()
 

option_hedging/code_pytorch/DeepHedgingEnvironment.py
@@ -16,9 +16,9 @@ from neural_networks.Transformer import Transformer
 
 class DeepHedgingEnvironment():
     
-    def __init__(self, model_type, nbs_point_traj, batch_size, r_borrow, r_lend, stock_dyn, params_vect, S_0, T, alpha, beta,
+    def __init__(self, model_type, nbs_point_traj, r_borrow, r_lend, stock_dyn, params_vect, S_0, T, alpha, beta,
                  loss_type, option_type, position_type, strike, V_0, nbs_layers, nbs_units, lr, dropout, prepro_stock,
-                 nbs_shares, lambdas, light, train_set, test_set, name='model'):
+                 nbs_shares, lambdas, light, train_set, test_set, discretized = False, name='model'):
         
         self.model_type = model_type
         self.nbs_point_traj = nbs_point_traj
@@ -52,6 +52,7 @@ class DeepHedgingEnvironment():
         self.light = light
         
         self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.discretized = discretized                                                                      # For if the action space is discretized
         self.discretized_actions = torch.arange(start=-0.5, end=2.0, step=0.05, device=self.device)
 
         self.train_set = train_set
@@ -110,9 +111,9 @@ class DeepHedgingEnvironment():
         Returns:
         - self.input_t: torch.Tensor of size [self.batch_size, 6] or [self.batch_size, 3] depending on the number of features. Features (state) to be input into the neural network.
         """
-        self.t = 0                      # the time step in the path
-        self.done = False               # variable which determines if the path (episode) is done
-        self.batch_size = batch_size    # Set the batch size of the environment samples (size 1 if using replay memory buffer)
+        self.t = 0                                                                  # the time step in the path
+        self.batch_size = batch_size                                                # Set the batch size of the environment samples (size 1 if using replay memory buffer)
+        self.done = torch.zeros(self.batch_size, device=self.device)                # variable which determines if the path (episode) is done
 
         # Different models' features
         # if self.model_type == "LSTM" or self.model_type == "GRU":
@@ -190,7 +191,10 @@ class DeepHedgingEnvironment():
         #     self.delta_t_next = self.model(input_t_seq, input_t_seq_mask)
 
         # When actions are discretized
-        self.delta_t_next = self.discretized_actions[action]                            # torch.Tensor of size [self.batch_size]. Action to be taken at next time step     
+        if self.discretized:
+            self.delta_t_next = self.discretized_actions[action]                            # torch.Tensor of size [self.batch_size]. Action to be taken at next time step     
+        else:
+            self.delta_t_next = action.flatten()
         # Once the hedge is computed, update M_t (cash reserve)
         if self.t == 0:
             diff_delta_t = self.delta_t_next                                            # torch.Tensor of size [self.batch_size]. Difference in hedging position from last period
@@ -221,7 +225,7 @@ class DeepHedgingEnvironment():
 
         batch = self.dataset[self.path*self.batch_size:(self.path+1)*self.batch_size,:]     # torch.Tensor of size [self.batch_size, self.N] representing a batch of price paths
         self.S_t = batch[:,self.t+1].to(device=self.device)                                 # torch.Tensor of size [self.batch_size] representing a batch of prices at the next time step
-
+        
         # Liquidation portfolio value
         L_t = self.liquid_func(self.S_t, self.delta_t_next, self.A_t, self.B_t)     # torch.Tensor of size [self.batch_size]. Revenue from liquidating
         self.V_t = self.int_rate_bank(self.M_t) + L_t                               # torch.Tensor of size [self.batch_size]. Portfolio value.
@@ -243,7 +247,7 @@ class DeepHedgingEnvironment():
             self.done = torch.ones(self.batch_size)                             #   set done to be True
             self.path += 1                                                      #   iterate path index
             self.t = 0                                                          #   set time step to 0
-            if (self.path+1)*self.batch_size > self.dataset.shape[0]-1:        #   if path is the final path in the dataset
+            if (self.path)*self.batch_size > self.dataset.shape[0]-1:           #   if path is the final path in the dataset
                 self.path = 0                                                   #       reset path counter
             # Compute hedging error at maturity
             # Check if worth it to execute or not
@@ -279,8 +283,8 @@ class DeepHedgingEnvironment():
             # print("HEDGING ERROR SHAPE: ", self.hedging_error.shape)
             # print("HEDGING ERROR: ", self.hedging_error)
         else:
-            self.hedging_error = torch.zeros(self.batch_size)
-            self.done = torch.zeros(self.batch_size)
+            self.hedging_error = torch.zeros(self.batch_size, device=self.device)
+            self.done = torch.zeros(self.batch_size, device=self.device)
 
         # update delta_t
         self.delta_t = self.delta_t_next

option_hedging/code_pytorch/Deep_Hedging_Experiments.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 import Utils_general
 import DeepHedgingEnvironment
 import RL_algorithms.DQN as DQN
+import RL_algorithms.PG as PG
 from data_generation_processes.GARCH import GARCH
 from scipy.stats import ttest_ind
 from scipy.stats import f
@@ -21,8 +22,8 @@ alpha = 1.00
 beta = 1.00
 
 batch_size = 512
-train_size = 2**17
-test_size = 2**17
+train_size = 2**20
+test_size = 2**20
 epochs = 1
 r_borrow = 0
 r_lend = 0
@@ -97,9 +98,9 @@ def generate_garch_dataset(dataset_type="train_set", size=train_size):
     torch.save(dataset, global_path_prefix + str(dataset_type))
 
 """Training the garch model and generating the datasets"""
-train_garch()
-generate_garch_dataset(dataset_type="train_set", size=train_size)
-generate_garch_dataset(dataset_type="test_set", size=test_size)
+# train_garch()
+# generate_garch_dataset(dataset_type="train_set", size=train_size)
+# generate_garch_dataset(dataset_type="test_set", size=test_size)
 
 # Select the device
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -190,15 +191,38 @@ batch_size = 64 | nbs_layers = 5 | nbs_units = 1024
 # rsmse_gru = np.sqrt(np.mean(semi_square_hedging_err_gru))
 
 # Initialize Deep Hedging environement
-deep_hedging_env = DeepHedgingEnvironment.DeepHedgingEnvironment("FFNN", nbs_point_traj, batch_size, r_borrow, r_lend, stock_dyn, params_vect, S_0, T, alpha, beta,
+deep_hedging_env = DeepHedgingEnvironment.DeepHedgingEnvironment("FFNN", nbs_point_traj, r_borrow, r_lend, stock_dyn, params_vect, S_0, T, alpha, beta,
             loss_type, option_type, position_type, strike, V_0, num_layers, nbs_units, lr, dropout, prepro_stock,
-            nbs_shares, lambdas, light, train_set=train_set, test_set=test_set, name=name_ffnn)
+            nbs_shares, lambdas, light, train_set=train_set, test_set=test_set, discretized= True, name=name_ffnn)
 
 # Train and test DQN model
-# all_losses_ffnn, ffnn_losses = agent_ffnn.train(train_set=train_set, train_size = train_size, epochs=epochs, lr_schedule=lr_schedule)
+
 dqn_agent = DQN.DoubleDQN(state_size=6, action_size=50, num_layers=num_layers, hidden_size=nbs_units, lr=lr, batch_size=batch_size)
-DQN.train_dqn(dqn_agent, deep_hedging_env)
-DQN.test_dqn(dqn_agent, deep_hedging_env)
+dqn_agent.train(deep_hedging_env, episodes= 500)
+dqn_actions, dqn_rewards = dqn_agent.test(deep_hedging_env)
+
+dqn_actions = dqn_actions.cpu().detach().numpy()
+dqn_rewards = dqn_rewards.cpu().detach().numpy()
+
+print(" ----------------- ")
+print(" DQN Results")
+print(" ----------------- ")
+Utils_general.print_stats(dqn_rewards, dqn_actions, "RSMSE", "DQN", V_0)
+
+# Train and test PG model
+
+# deep_hedging_env.discretized = False
+# pg_agent = PG.PG(state_size=6, action_size=1, num_layers=num_layers, hidden_size=nbs_units, lr=lr, batch_size=batch_size)
+# pg_agent.train(deep_hedging_env, episodes=3000)
+# pg_actions, pg_rewards = pg_agent.test(deep_hedging_env)
+
+# pg_actions = pg_actions.cpu().detach().numpy()
+# pg_rewards = pg_rewards.cpu().detach().numpy()
+
+# print(" ----------------- ")
+# print(" Policy Gradient Results")
+# print(" ----------------- ")
+# Utils_general.print_stats(pg_rewards, pg_actions, "RSMSE", "Policy Gradient", V_0)
 
 # agent_ffnn.model = torch.load(name_ffnn)
 # deltas_ffnn, hedging_err_ffnn, S_t_ffnn, V_t_ffnn, A_t_ffnn, B_t_ffnn = agent_ffnn.test(test_size=test_size, test_set=test_set)
@@ -236,8 +260,6 @@ print(" ----------------- ")
 print(" Delta Hedging Results")
 print(" ----------------- ")
 deltas_DH, hedging_err_DH = Utils_general.delta_hedge_res(test_set, r_borrow, r_lend, params_vect[1], T, alpha, beta, option_type=option_type, position_type=position_type, strike=strike, V_0=V_0, nbs_shares=nbs_shares, hab=lambdas)
-print("deltas_DH.shape: ", deltas_DH.shape)
-print("hedging_err_DH.shape: ", hedging_err_DH.shape)
 Utils_general.print_stats(hedging_err_DH, deltas_DH, "Delta hedge", "Delta hedge", V_0)
 semi_square_hedging_err_DH = np.square(np.where(hedging_err_DH > 0, hedging_err_DH, 0))
 smse_DH = np.mean(semi_square_hedging_err_DH)