34708029da05aad4287cfbfffb4ee9c9812fe7b8
Andrei-T-Neagu <neagu.andrei33@gmail.com>
2026-01-12 14:57:58 -0500

    update to ppo log clamp

--- neural_networks/FFNN.py
+++ neural_networks/FFNN.py
@@ -39,7 +39,7 @@ class FFNN(nn.Module):
         else:
             x = self.final_linear(x)
             if self.log_predicted:
-                x = torch.clamp(x, -10, 2)
+                x = torch.clamp(x, -5, 2)
             if self.policy:
                 x = torch.clamp(x, 0.0, 1.0)
             if self.value:
