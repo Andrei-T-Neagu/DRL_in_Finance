06c157f4856e5f68f5963d3b5574cf507facb843
Andrei Neagu <a_eagu@speed-submit2.encs.concordia.ca>
2026-02-13 12:45:25 -0500

    td3 tuned

--- option_hedging/Deep_Hedging_Experiments.py
+++ option_hedging/Deep_Hedging_Experiments.py
@@ -29,7 +29,7 @@ from scipy.stats import ttest_ind
 start_total_time = datetime.datetime.now()
 episodes = 200000
 trans_costs = 0.00              #proportional transaction costs 0.0 or 0.01
-twin_delayed=False
+twin_delayed=True
 double=False
 dueling=False
 T = 252/252
